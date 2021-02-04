95e6f424e0ab84c659b6facee4a86f9ab5c8eb4c
Abhinav Gangula <gangulaabhinav@gmail.com>
2021-02-05 00:39:59 +0530

    Enable mixed mode debugging. Adjusting build dependencies. WebApplication2 is dependent on CppLibrary. Adjusting solution configurations. Using Win32 versiosn of CppLibrary.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Runtime.InteropServices;
 
 namespace WebApplication2.Controllers
 {
     public class HomeController : Controller
     {
+        [DllImport("CppLibrary.dll")]
+        public static extern void Hello();
+
         public ActionResult Index()
         {
             return View();
         }
         public ActionResult Cpp()
         {
-            ViewBag.Title = "About";
-            ViewBag.Message = "About AspDotNetCppThreejs.";
+            ViewBag.Title = "Cpp";
+            ViewBag.Message = "Calling Hello() from CppLibrary.dll";
+            Hello();
             return View();
         }
         public ActionResult About()
--- Readme.md
+++ Readme.md
@@ -2,3 +2,4 @@
 https://www.c-sharpcorner.com/UploadFile/1e050f/creating-first-application-in-Asp-Net-mvc-4-0/
 https://www.c-sharpcorner.com/UploadFile/3d39b4/Asp-Net-mvc-4-layout-and-section-in-razor/
 Project Setup and Hello World - P/INVOKE WITH C# AND C++ TUTORIAL #1 - https://www.youtube.com/watch?v=NKIdxJAbr0Q
+https://docs.microsoft.com/en-us/visualstudio/debugger/how-to-debug-managed-and-native-code?view=vs-2019

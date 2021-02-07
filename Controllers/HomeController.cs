464652b26d61fca730231458df4cac3be2be1232
Abhinav Gangula <gangulaabhinav@gmail.com>
2021-02-07 10:51:13 +0530

    Add HomeController. Returning string from Home/Index and Empty View from Home/About.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspDotNetCppThreejs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Sample String from HomeController";
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}

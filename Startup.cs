76984c6654ec40494afc99b899f22a93ed6d532b
Abhinav Gangula <abgangul@microsoft.com>
2021-06-06 00:04:44 +0530

    Commit basic Razor Project

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,9 @@
+using AspDotNetCppThreejs.Data;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,11 +15,20 @@ namespace AspDotNetCppThreejs
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,16 +38,22 @@ namespace AspDotNetCppThreejs
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
             });
         }
     }
--- build_cpplibrary.bat
+++
@@ -1,4 +0,0 @@
-echo Building CppLibrary.vcxproj to the location %DEPLOYMENT_TEMP%
-IF DEFINED DEPLOYMENT_TEMP (
-    msbuild CppLibrary\CppLibrary.vcxproj /p:configuration=Release /p:platform=win32 /p:OutDir="%DEPLOYMENT_TEMP%"
-)
